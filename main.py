4b742ee14020d60b06280eb3eaea8d469f838d87
alii2 <ihsan0974@gmail.com>
2024-04-26 14:11:08 +0300

    başlangıç

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 import pickle
-import numpy as np
 import cv2
 import face_recognition
 import cvzone
@@ -41,13 +40,23 @@ while True:
 
 
     imgBackground[162:162+480,55:55+640] = img
-    imgBackground[44:44+633,808:808+414] = imgModeList[0]
+    imgBackground[44:44+633,808:808+414] = imgModeList[3]
 
-    for encodeFace , faceLoc in zip(encodeCurFrame, faceCurFrame):
+    for encodeFace, faceLoc in zip(encodeCurFrame, faceCurFrame):
         matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
         faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
-        print("matches", matches)
-        print("faceDis", faceDis)
+
+        matchIndex= np.argmin(faceDis)
+        #print("match ındex",matchIndex)
+
+        if matches[matchIndex]:
+            # print("Known Face Detected")
+            # print(studentIds[matchIndex])
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+            bbox = 55 + x1, 162 + y1, x2 - x1, y2 - y1
+            imgBackground = cvzone.cornerRect(imgBackground, bbox, rt=0)
+
 
 
 
