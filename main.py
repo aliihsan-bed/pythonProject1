0a1f9bb6c6d8e62e6ba22b85838c632258e22711
alii2 <ihsan0974@gmail.com>
2024-04-26 14:40:47 +0300

    başlangıç

--- main.py
+++ main.py
@@ -7,47 +7,48 @@ import numpy as np
 from datetime import datetime
 
 cap = cv2.VideoCapture(0)
-cap.set(3,640)
-cap.set(4,480)
+cap.set(3, 640)
+cap.set(4, 480)
 
 imgBackground = cv2.imread('Resources/background.png')
 
-# importing the mode images into a list
-folderModePath='Resources/Modes'
-modePathList=os.listdir(folderModePath)
-imgModeList=[]
+# Importing the mode images into a list
+folderModePath = 'Resources/Modes'
+modePathList = os.listdir(folderModePath)
+imgModeList = []
 for path in modePathList:
-    imgModeList.append(cv2.imread((os.path.join(folderModePath,path))))
+    imgModeList.append(cv2.imread(os.path.join(folderModePath, path)))
+# print(len(imgModeList))
 
 # Load the encoding file
-
-print("Loading encode file ...")
+print("Loading Encode File ...")
 file = open('EncodeFile.p', 'rb')
 encodeListKnownWithIds = pickle.load(file)
 file.close()
-encodeListKnown, stundetIds = encodeListKnownWithIds
-
-print("Encode file loaded...")
+encodeListKnown, studentIds = encodeListKnownWithIds
+# print(studentIds)
+print("Encode File Loaded")
 
 while True:
     success, img = cap.read()
 
-    imgS = cv2.resize(img, (0,0), None, 0.25, 0.25)
-    imgS = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
     faceCurFrame = face_recognition.face_locations(imgS)
     encodeCurFrame = face_recognition.face_encodings(imgS, faceCurFrame)
 
-
-    imgBackground[162:162+480,55:55+640] = img
-    imgBackground[44:44+633,808:808+414] = imgModeList[3]
+    imgBackground[162:162 + 480, 55:55 + 640] = img
+    imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[3]
 
     for encodeFace, faceLoc in zip(encodeCurFrame, faceCurFrame):
         matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
         faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+        # print("matches", matches)
+        # print("faceDis", faceDis)
 
-        matchIndex= np.argmin(faceDis)
-        #print("match ındex",matchIndex)
+        matchIndex = np.argmin(faceDis)
+        # print("Match Index", matchIndex)
 
         if matches[matchIndex]:
             # print("Known Face Detected")
@@ -65,4 +66,3 @@ while True:
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
