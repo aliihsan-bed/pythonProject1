08438678d20656f04c6f0d5714878fa2b1fa4c7a
alii2 <ihsan0974@gmail.com>
2024-07-16 23:12:05 +0300

    başlangıKKç

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ imgStudent = []
 
 while True:
     success, img = cap.read()
-    img = cv2.flip(img, 1)  # Yatay olarak çevir
+    img = cv2.flip(img, 1)  # Yatay olarak çevirme
     imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
